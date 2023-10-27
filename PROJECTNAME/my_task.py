66de05e0eda3485e758b7386ac2f97fbc147ef65
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-10-27 16:30:36 +0200

    Create my_task.py

---
+++ PROJECTNAME/my_task.py
@@ -0,0 +1,10 @@
+def my_task(arg1: int) -> int:
+    """
+    This is the short description
+
+    This is a long description. This is a long description. This is a long description. This is a long description. This is a long description.
+
+    Args:
+        arg1: description
+    """
+    return 1
