72e949d9029e1d60ac64456ba0c4595b2fbaeae1
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-10-30 10:01:54 +0100

    Update my_task.py with pydantic and fractal interfaces

--- PROJECTNAME/my_task.py
+++ PROJECTNAME/my_task.py
@@ -1,10 +1,24 @@
+"""
+This is the Python module for my_task
+"""
+
+from pydantic.decorator import validate_arguments
+
+@validate_arguments
 def my_task(arg1: int) -> int:
     """
     This is the short description
 
-    This is a long description. This is a long description. This is a long description. This is a long description. This is a long description.
+    This is a long description. This is a long description. This is a long
+    description. This is a long description. This is a long description.
 
     Args:
         arg1: description
     """
     return 1
+
+
+if __name__ == "__main__":
+    from fractal_tasks_core.tasks._utils import run_fractal_task
+
+    run_fractal_task(task_function=my_task)
